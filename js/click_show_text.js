ae24e408f28e30448f6ecb4bf86b91c1527217ec
MacsedProtoss <yu410420@gmail.com>
2019-05-27 22:06:19 +0800

    Site updated: 2019-05-27 22:06:18

---
+++ js/click_show_text.js
@@ -0,0 +1,32 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array
+        ("Stay Hungry", "Stay Foolish", "Think Different", "Macsed Protoss",  "Geeker", "iOS Developer");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#F4F4F4"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+			3000,
+			function() {
+			    $i.remove();
+			});
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+   
